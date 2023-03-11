5d02833b7d6017a7dbfa72b427dcb9433669ade8
baguscs <cahyob59@gmail.com>
2023-03-11 18:01:44 +0700

    fix bug view mail account

--- resources/views/template/akun/send-email.blade.php
+++ resources/views/template/akun/send-email.blade.php
@@ -118,7 +118,7 @@
 																<div align="center" class="alignment"
 																	style="line-height:10px"><img
 																		alt="Card Header with Border and Shadow Animated"
-																		class="big" src="{{ asset('images/animated_header.gif') }}"
+																		class="big" src="https://d1oco4z2z1fhwp.cloudfront.net/templates/default/3991/animated_header.gif"
 																		style="display: block; height: auto; border: 0; width: 600px; max-width: 100%;"
 																		title="Card Header with Border and Shadow Animated"
 																		width="600" /></div>
@@ -135,7 +135,7 @@
 					</table>
 					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-2"
 						role="presentation"
-						style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #d9dffa; background-image: url('images/body_background_2.png'); background-position: top center; background-repeat: repeat;"
+						style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #d9dffa; background-image: url('https://d1oco4z2z1fhwp.cloudfront.net/templates/default/3991/body_background_2.png'); background-position: top center; background-repeat: repeat;"
 						width="100%">
 						<tbody>
 							<tr>
@@ -216,7 +216,9 @@
 																		style="font-size: 12px; mso-line-height-alt: 14.399999999999999px; color: #40507a; line-height: 1.2; font-family: Helvetica Neue, Helvetica, Arial, sans-serif;">
 																		<p dir="ltr"
 																			style="margin: 0; font-size: 12px; mso-line-height-alt: 14.399999999999999px;">
-																			Nama : <strong>{{ $mailData['name'] }}</strong>
+																			Nama : <strong>
+																				{{ $mailData['name'] }}
+																			</strong>
 																		</p>
 																	</div>
 																</div>
@@ -234,7 +236,9 @@
 																		style="font-size: 12px; mso-line-height-alt: 14.399999999999999px; color: #40507a; line-height: 1.2; font-family: Helvetica Neue, Helvetica, Arial, sans-serif;">
 																		<p dir="ltr"
 																			style="margin: 0; font-size: 12px; mso-line-height-alt: 14.399999999999999px;">
-																			E-Mail : <strong>{{ $mailData['email'] }}</strong>
+																			E-Mail : <strong>
+																				{{ $mailData['email'] }}
+																			</strong>
 																		</p>
 																	</div>
 																</div>
@@ -253,6 +257,7 @@
 																		<p
 																			style="margin: 0; font-size: 12px; mso-line-height-alt: 14.399999999999999px;">
 																			Password : <strong>{{ $mailData['password'] }}</strong></p>
+																			
 																	</div>
 																</div>
 															</td>
@@ -308,7 +313,7 @@
 																<div align="center" class="alignment"
 																	style="line-height:10px"><img
 																		alt="Card Bottom with Border and Shadow Image"
-																		class="big" src="images/bottom_img.png"
+																		class="big" src="https://d1oco4z2z1fhwp.cloudfront.net/templates/default/3991/bottom_img.png"
 																		style="display: block; height: auto; border: 0; width: 600px; max-width: 100%;"
 																		title="Card Bottom with Border and Shadow Image"
 																		width="600" /></div>
@@ -337,23 +342,6 @@
 												<td class="column column-1"
 													style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 20px; padding-left: 10px; padding-right: 10px; padding-top: 10px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"
 													width="100%">
-													<table border="0" cellpadding="10" cellspacing="0"
-														class="image_block block-1" role="presentation"
-														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
-														width="100%">
-														<tr>
-															<td class="pad">
-																<div align="center" class="alignment"
-																	style="line-height:10px"><a
-																		href="http://www.example.com/"
-																		style="outline:none" tabindex="-1"
-																		target="_blank"><img alt="Your Logo"
-																			src="images/logo.png"
-																			style="display: block; height: auto; border: 0; width: 145px; max-width: 100%;"
-																			title="Your Logo" width="145" /></a></div>
-															</td>
-														</tr>
-													</table>
 													<table border="0" cellpadding="10" cellspacing="0"
 														class="text_block block-2" role="presentation"
 														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
@@ -386,74 +374,6 @@
 																				2023 SIMPELAE.</span></p>
 																	</div>
 																</div>
-															</td>
-														</tr>
-													</table>
-												</td>
-											</tr>
-										</tbody>
-									</table>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-5"
-						role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
-						<tbody>
-							<tr>
-								<td>
-									<table align="center" border="0" cellpadding="0" cellspacing="0"
-										class="row-content stack" role="presentation"
-										style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 600px;"
-										width="600">
-										<tbody>
-											<tr>
-												<td class="column column-1"
-													style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"
-													width="100%">
-													<table border="0" cellpadding="0" cellspacing="0"
-														class="icons_block block-1" role="presentation"
-														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
-														width="100%">
-														<tr>
-															<td class="pad"
-																style="vertical-align: middle; color: #9d9d9d; font-family: inherit; font-size: 15px; padding-bottom: 5px; padding-top: 5px; text-align: center;">
-																<table cellpadding="0" cellspacing="0"
-																	role="presentation"
-																	style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
-																	width="100%">
-																	<tr>
-																		<td class="alignment"
-																			style="vertical-align: middle; text-align: center;">
-																			<!--[if vml]><table align="left" cellpadding="0" cellspacing="0" role="presentation" style="display:inline-block;padding-left:0px;padding-right:0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;"><![endif]-->
-																			<!--[if !vml]><!-->
-																			<table cellpadding="0" cellspacing="0"
-																				class="icons-inner" role="presentation"
-																				style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block; margin-right: -4px; padding-left: 0px; padding-right: 0px;">
-																				<!--<![endif]-->
-																				<tr>
-																					<td
-																						style="vertical-align: middle; text-align: center; padding-top: 5px; padding-bottom: 5px; padding-left: 5px; padding-right: 6px;">
-																						<a href="https://www.designedwithbee.com/"
-																							style="text-decoration: none;"
-																							target="_blank"><img
-																								align="center"
-																								alt="Designed with BEE"
-																								class="icon" height="32"
-																								src="images/bee.png"
-																								style="display: block; height: auto; margin: 0 auto; border: 0;"
-																								width="34" /></a></td>
-																					<td
-																						style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif; font-size: 15px; color: #9d9d9d; vertical-align: middle; letter-spacing: undefined; text-align: center;">
-																						<a href="https://www.designedwithbee.com/"
-																							style="color: #9d9d9d; text-decoration: none;"
-																							target="_blank">Designed
-																							with BEE</a></td>
-																				</tr>
-																			</table>
-																		</td>
-																	</tr>
-																</table>
 															</td>
 														</tr>
 													</table>
